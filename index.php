5b26356a490fa455bc1047766e9b0a1eb3c874e3
InosDev <inosbog@gmail.com>
2025-02-10 10:30:56 +0300

    2 commit

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 
 $arr = [10, 5];
 
-fwrite($stdin, $arr[0], $arr[1]);
+$stdin = fopen('php://stdin', $arr[0], $arr[1]);
 
-if (is_int($arr[0]) && is_int($arr[1])) {
+if (is_numeric($arr[0]) && is_numeric($arr[1])) {
 
     if ($arr[1] = 0 ){
 
         fscanf(STDERR, "%d\n", "Делить на 0 нельзя");
         
     } else { 
+    	
 
         $quotient = $arr[0] / $arr[1];
 
